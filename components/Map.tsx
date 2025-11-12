6784f3303cf47c43b619b2cc7cbb25b623386ea3
Dino0204 <s24020@gsm.hs.kr>
2025-11-12 22:45:09 +0900

    feat: map

---
+++ components/Map.tsx
@@ -0,0 +1,25 @@
+import { PointerLockControls, Sky } from "@react-three/drei";
+import { Physics, RigidBody } from "@react-three/rapier";
+import Ground from "./Ground";
+import Player from "./Player";
+import Box from "./Box";
+
+export default function Map() {
+
+
+
+  return (
+    <>
+      <PointerLockControls />
+      <Sky sunPosition={[100, 20, 100]} />
+      <ambientLight intensity={1.5} />
+
+      <Physics gravity={[0, -20, 0]}>
+        <Box />
+        <Player />
+        <Ground />
+      </Physics>
+
+    </>
+  )
+}
